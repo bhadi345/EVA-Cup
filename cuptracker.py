f4264b7688cd5a712b1fb3a5682a69f2ec9a6b49
bhadi345 <bhadi8960@gmail.com>
2026-06-12 00:41:34 +0200

    Update webhook message content for EU Cups

--- cuptracker.py
+++ cuptracker.py
@@ -2,9 +2,17 @@ import requests
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
 
+text = """
+🏆 Nächste EU Cups
+
+🎯 Solo Victory Cup
+🕒 19:00 Uhr
+
+🎯 Duo Cash Cup
+🕒 20:00 Uhr
+"""
+
 requests.post(
     WEBHOOK_URL,
-    json={
-        "content": "🏆 TEST: EVA Cup Tracker funktioniert!"
-    }
+    json={"content": text}
 )
