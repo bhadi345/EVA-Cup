2b272f5576f1d85ffa4ef8d20d347236a6776856
bhadi345 <bhadi8960@gmail.com>
2026-06-12 00:43:21 +0200

    Update notification message for EU Cups

--- cuptracker.py
+++ cuptracker.py
@@ -3,7 +3,7 @@ import requests
 WEBHOOK_URL = "https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
 
 text = """
-🏆 Nächste EU Cups
+@everyone 🏆 Nächste EU Cups
 
 🎯 Solo Victory Cup
 🕒 19:00 Uhr
