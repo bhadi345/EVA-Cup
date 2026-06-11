b1808a81fb3a8feed9c67dfde940d84a3a46ba2a
bhadi345 <bhadi8960@gmail.com>
2026-06-12 00:52:22 +0200

    Change webhook URL to Fortnite Tracker events
    Updated webhook URL to fetch events from Fortnite Tracker.

--- cuptracker.py
+++ cuptracker.py
@@ -1,18 +1,8 @@
 import requests
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
+url = "https://fortnitetracker.com/events.ics?region=EU"
 
-text = """
-@everyone 🏆 Nächste EU Cups
+r = requests.get(url)
 
-🎯 Solo Victory Cup
-🕒 19:00 Uhr
-
-🎯 Duo Cash Cup
-🕒 20:00 Uhr
-"""
-
-requests.post(
-    WEBHOOK_URL,
-    json={"content": text}
+print(r.text[:500])
 )
