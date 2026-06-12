84dccb48dbc62fe98fba1592fff34301925d13b3
bhadi345 <bhadi8960@gmail.com>
2026-06-12 12:22:34 +0200

    Refactor API request and webhook message format

--- cuptracker.py
+++ cuptracker.py
@@ -1,31 +1,21 @@
 import requests
 
-API_KEY = "14eb41dafca37dc846edbcf65aa3678fbee1268ec4aa8956f58c8de83b5b3c66"
-WEBHOOK = "https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
+API_KEY="14eb41dafca37dc846edbcf65aa3678fbee1268ec4aa8956f58c8de83b5b3c66"
+WEBHOOK="https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
 
-url = "https://prod.api-fortnite.com/api/v1/events/tracker"
+url="https://prod.api-fortnite.com/api/v1/events/tracker"
 
-headers = {
-    "Authorization": API_KEY
+headers={
+    "x-api-key": API_KEY
 }
 
-r = requests.get(url, headers=headers)
-
-msg = f"""
-🏆 EVA Cup Test
-
-Status:
-{r.status_code}
-
-Erste Daten:
-{r.text[:500]}
-"""
+r=requests.get(url,headers=headers)
 
 requests.post(
     WEBHOOK,
     json={
-        "content": msg
+        "content": f"🏆 TEST\n\nStatus: {r.status_code}\n\n{r.text[:500]}"
     }
 )
 
-print("Gesendet")
+print(r.status_code)
