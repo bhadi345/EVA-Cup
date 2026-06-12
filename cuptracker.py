a94af0e6602931f5e812174bee0e01fac16bcda2
bhadi345 <bhadi8960@gmail.com>
2026-06-12 12:43:34 +0200

    Update event message for EU Cups in cuptracker.py
    Refactor event message formatting and add event details for EU Cups.

--- cuptracker.py
+++ cuptracker.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 API_KEY="14eb41dafca37dc846edbcf65aa3678fbee1268ec4aa8956f58c8de83b5b3c66"
 WEBHOOK="https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
@@ -6,20 +7,44 @@ WEBHOOK="https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2T
 url="https://prod.api-fortnite.com/api/v1/events/global"
 
 headers={
-    "x-api-key": API_KEY
+    "x-api-key":API_KEY
 }
 
 r=requests.get(url,headers=headers)
 
-data=r.json()
+events=r.json()
 
-msg=f"""
-🏆 EVA Event Test
+msg="@everyone\n\n🏆 **Nächste EU Cups**\n\n"
 
-Status: {r.status_code}
+count=0
 
-{str(data)[:1200]}
-"""
+for event in events:
+
+    if "EU" in event.get("regions", {}):
+
+        for cup in event["regions"]["EU"]:
+
+            name=event.get("titleLine1","Cup")
+
+            start=cup["beginTime"]
+
+            dt=datetime.fromisoformat(
+                start.replace("Z","+00:00")
+            )
+
+            zeit=dt.strftime("%d.%m • %H:%M UTC")
+
+            msg+=f"🎯 {name}\n🕒 {zeit}\n\n"
+
+            count+=1
+
+            if count>=5:
+                break
+
+    if count>=5:
+        break
+
+msg+="💙 EVA Esports"
 
 requests.post(
 WEBHOOK,
