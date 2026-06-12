fd56bc3178a8b4174a0b9eaf5f40664db6c8aab4
bhadi345 <bhadi8960@gmail.com>
2026-06-12 12:18:01 +0200

    Integrate Discord webhook for API response notifications
    Send a message to Discord webhook with API response details.

--- cuptracker.py
+++ cuptracker.py
@@ -1,6 +1,7 @@
 import requests
 
 API_KEY = "14eb41dafca37dc846edbcf65aa3678fbee1268ec4aa8956f58c8de83b5b3c66"
+WEBHOOK = "https://discord.com/api/webhooks/1514757021600055366/o67iC60ZiYFKETub2TqjRt8Cvikl78x_4N7QVmacte7kHs-AmvovGUVJAKha_PLBkiIe"
 
 url = "https://prod.api-fortnite.com/api/v1/events/tracker"
 
@@ -10,5 +11,21 @@ headers = {
 
 r = requests.get(url, headers=headers)
 
-print(r.status_code)
-print(r.text[:2000])
+msg = f"""
+🏆 EVA Cup Test
+
+Status:
+{r.status_code}
+
+Erste Daten:
+{r.text[:500]}
+"""
+
+requests.post(
+    WEBHOOK,
+    json={
+        "content": msg
+    }
+)
+
+print("Gesendet")
